ce5699342836b6b828e9c43ab175f9185cc62b3d
Ryan Pate <fanatical@ufo.exchange>
2022-11-29 12:20:15 -0500

    Bash variables.

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME="Jimbob"
+HATSIZE="7.5"
+
+# Single quotes won't print the variable's value:
+echo 'Hello, my name is $NAME.'
+# Double quotes will.
+echo "Hello, my name is $NAME."
+echo "My hatsize is $HATSIZE."
