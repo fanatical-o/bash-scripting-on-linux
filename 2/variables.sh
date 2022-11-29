b5435fa68e994880d3d761eca233ade3bf48e10f
Ryan Pate <fanatical@ufo.exchange>
2022-11-29 12:32:26 -0500

    Move scripts into directories by class number.

--- 2/variables.sh
+++ 2/variables.sh
@@ -8,3 +8,14 @@ echo 'Hello, my name is $NAME.'
 # Double quotes will.
 echo "Hello, my name is $NAME."
 echo "My hatsize is $HATSIZE."
+
+echo "---"
+# Sometimes we use variables to repetition
+WORD="awesome"
+
+echo "Linux is $WORD"
+echo "Sunny days are $WORD"
+echo "Tea is $WORD"
+
+echo "---"
+
