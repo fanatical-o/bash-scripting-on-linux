06b7de1f52cb4f4b891146f7ac679ec286cc64fd
Ryan Pate <fanatical@ufo.exchange>
2022-11-29 13:21:13 -0500

    Use the 'command' command.

--- 4/doescommandexist.sh
+++ 4/doescommandexist.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-CMD=/usr/bin/htop
-
-if [ -f $CMD ] 
+CMD=htop
+# Check for the existence of our CMD variable
+# using command, similar to which
+if command -v $CMD 
 then
     echo "$CMD is available, running..."
 else
     echo "$CMD command is not available, installing..."
     # Apt package manager is Debian specific
-    sudo apt update && sudo apt install -y htop
+    sudo apt update && sudo apt install -y $CMD
 fi
 # Run the command
 $CMD
