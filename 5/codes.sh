7a4a7b360a2a16266d3efca8c0c66e1dcf514dc8
Ryan Pate <fanatical@ufo.exchange>
2022-11-30 08:53:08 -0500

    Use specific exit codes to break out of a program.

--- 5/codes.sh
+++ 5/codes.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 
-DIR=/notexist
+DIR=/etc
 
 if [ -d $DIR ] 
 then
-    # echo $?
     echo "The directory $DIR exists."
+    exit 0
 else
     # echo $?
     # Even though the directory wasn't found, the program
     # successfully echoes this message, so the exit code
     # will be 0
     echo "The directory $DIR does not exist."
+    exit 1
 fi
 
 echo "The exit code for this script is: $?"
