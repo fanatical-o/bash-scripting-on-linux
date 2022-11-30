3abfb4e08e3f32235c3f96c56e1864bbc5db5ef4
Ryan Pate <fanatical@ufo.exchange>
2022-11-30 17:44:28 -0500

    Check for errors. Redirect stdout and stderr to a log file and error file, respectively.

--- 7/universal-update.sh
+++ 7/universal-update.sh
@@ -2,12 +2,17 @@
 
 
 RELEASE_FILE=/etc/os-release
+LOGFILE=/var/log/updater.log
+ERRORLOG=/var/log/updater_error.log
 
 # grep /etc/os-release to find linux distribution 
 # Use quiet mode with grep
 if grep -q "Debian" $RELEASE_FILE ||  grep -q "Ubuntu" $RELEASE_FILE
 then
     # The host is Debian/Ubuntu, use apt
-    sudo apt update
+    sudo apt update 1>>$LOGFILE 2>>$ERRORLOG
+    if [ $? -ne 0 ]
+    then
+	echo "An error occurred, please check the $errorlog file."
     sudo apt dist-upgrade 
 fi
