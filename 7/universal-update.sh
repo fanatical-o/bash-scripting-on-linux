b8487f0eff6670f92e1fd541dfadce29b37f7b63
Ryan Pate <fanatical@ufo.exchange>
2022-11-30 18:11:04 -0500

    Define a function to check exit codes.

--- 7/universal-update.sh
+++ 7/universal-update.sh
@@ -5,18 +5,21 @@ RELEASE_FILE=/etc/os-release
 LOGFILE=/var/log/updater.log
 ERRORLOG=/var/log/updater_error.log
 
+# Is the exit code anything other than 0?
+check_exit_status() {
+    if [ $? -ne 0 ]
+    then
+	echo "An error occurred, please check the $errorlog file."
+    fi
+}
 # grep /etc/os-release to find linux distribution 
 # Use quiet mode with grep
 if grep -q "Debian" $RELEASE_FILE ||  grep -q "Ubuntu" $RELEASE_FILE
 then
     # Redirect stdout and stderr to disk
     sudo apt update 1>>$LOGFILE 2>>$ERRORLOG
-    # Is the exit code anything other than 0? 
-    if [ $? -ne 0 ]
-    then
-	echo "An error occurred, please check the $errorlog file."
-    sudo apt dist-upgrade -y
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file." 
+    check_exit_status
+
+    sudo apt dist-upgrade -y 1>>$LOGFILE 2>>$ERRORLOG
+    check_exit_status
 fi
