96968bf961a8e75f3223469ca7d4443ba12ca57d
Ryan Pate <fanatical@ufo.exchange>
2022-11-30 17:55:25 -0500

    Capture/use stdin, stdout and stderr. Update update script for the Data Streams class.

--- 7/universal-update.sh
+++ 7/universal-update.sh
@@ -9,10 +9,14 @@ ERRORLOG=/var/log/updater_error.log
 # Use quiet mode with grep
 if grep -q "Debian" $RELEASE_FILE ||  grep -q "Ubuntu" $RELEASE_FILE
 then
-    # The host is Debian/Ubuntu, use apt
+    # Redirect stdout and stderr to disk
     sudo apt update 1>>$LOGFILE 2>>$ERRORLOG
+    # Is the exit code anything other than 0? 
     if [ $? -ne 0 ]
     then
 	echo "An error occurred, please check the $errorlog file."
-    sudo apt dist-upgrade 
+    sudo apt dist-upgrade -y
+    if [ $? -ne 0 ]
+    then
+        echo "An error occurred, please check the $errorlog file." 
 fi
---
+++ 9/data-streams.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Capture stdin and echo back 
+echo "Please enter anything at all: "
+read anything
+echo "You typed: $anything"
