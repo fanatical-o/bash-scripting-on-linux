ebb0dbdcbd6aca6939e867a9d4465aef6b5ee0ec
Ryan Pate <fanatical@ufo.exchange>
2022-11-30 09:36:11 -0500

    Getting better all the time.

--- 7/universal-update.sh
+++ 7/universal-update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
 RELEASE_FILE=/etc/os-release
 
-# Does /etc/apt exist on the host?
+# grep /etc/os-release to find linux distribution 
 # Use quiet mode with grep
-if grep -q "Ubuntu" $RELEASE_FILE 
+if grep -q "Debian" $RELEASE_FILE ||  grep -q "Ubuntu" $RELEASE_FILE
 then
     # The host is Debian/Ubuntu, use apt
     sudo apt update
